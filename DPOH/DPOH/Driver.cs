00191dc6f8a76d4d4f61bda672ab982de73c5274
Brandon Stanley <bstanley@lakeheadu.ca>
2015-04-12 19:12:27 -0400

    Project Upload
    the complete project 3

---
+++ DPOH/DPOH/Driver.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="Driver.cs" company="engi">
+// The driver for the whole project.  Runs the DatabaseAccess methods.
+// </copyright>
+namespace DPOH
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Main driver, executes the various DatabaseAccess methods.
+    /// </summary>
+    public class Driver
+    {
+        /// <summary>
+        /// Main method of driver.  Drives the DatabaseAccess class
+        /// </summary>
+        /// <param name="args">args are not used.</param>
+        public static void Main(string[] args)
+        {
+            Tuple<long, double> temp = DatabaseAccess.RowCountAndExecutionTime(false); // non-indexed table
+            Console.WriteLine("Non-indexed row count: " + temp.Item1 + " and execution time: " + temp.Item2 + "ms");
+
+            temp = DatabaseAccess.RowCountAndExecutionTime(true); // indexed table
+            Console.WriteLine("Indexed row count: " + temp.Item1 + " and execution time: " + temp.Item2 + "ms");
+
+            Console.WriteLine("\n\nThe non-indexed execution plan is:\n\n" + DatabaseAccess.GetSelectQueryPlan(false));
+
+            Console.WriteLine("\n\nThe indexed execution plan is:\n\n" + DatabaseAccess.GetSelectQueryPlan(true));
+
+            Console.ReadLine();
+        }
+    }
+}
